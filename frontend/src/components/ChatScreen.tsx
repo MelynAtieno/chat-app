a95318df0438532615d4ee88bf537d8bc93f9fa6
Melyn Were <WereMA@cardiff.ac.uk>
2026-01-26 14:58:55 +0000

    feat: implement ChatScreen layout and add styling for text input

--- frontend/src/components/ChatScreen.tsx
+++ frontend/src/components/ChatScreen.tsx
@@ -1,6 +1,5 @@
-
 import { useNavigate } from 'react-router-dom';
-
+import '../styles/ChatScreen.css';
 
 
 function ChatScreen() {
@@ -9,10 +8,16 @@ function ChatScreen() {
     navigate('/');
   }
   return (
+    <>
     <div>
       <h1>Start Chatting!!!!!</h1>
       <button onClick = {handleBack}>BACK</button>
     </div>
+    <div>
+      <input className='text-input' type="text" placeholder="Type a message..." />
+      <button>SEND</button>
+    </div>
+    </>
   );
 }
 
