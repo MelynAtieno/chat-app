2e0804a8e7f43b9a23b129c61b72f20e3ef933e7
Melyn Were <WereMA@cardiff.ac.uk>
2026-01-26 15:12:30 +0000

    feat: enhance ChatScreen layout with structured sections and improved styling

--- frontend/src/components/ChatScreen.tsx
+++ frontend/src/components/ChatScreen.tsx
@@ -8,16 +8,24 @@ function ChatScreen() {
     navigate('/');
   }
   return (
-    <>
-    <div>
-      <h1>Start Chatting!!!!!</h1>
-      <button onClick = {handleBack}>BACK</button>
+    <div className="chat-container">
+      {/* Header Section - Top */}
+      <div className="chat-header">
+        <h1>Start Chatting!!!!!</h1>
+        <button onClick={handleBack}>BACK</button>
+      </div>
+
+      {/* Messages Section - Middle (expands to fill space) */}
+      <div className="chat-messages">
+        {/* Messages will go here */}
+      </div>
+
+      {/* Input Section - Bottom */}
+      <div className="chat-input-section">
+        <input className="text-input" type="text" placeholder="Type a message..." />
+        <button>SEND</button>
+      </div>
     </div>
-    <div>
-      <input className='text-input' type="text" placeholder="Type a message..." />
-      <button>SEND</button>
-    </div>
-    </>
   );
 }
 
