c8aa7097ecc0499eb5b4fbd7ab156ae535b74496
Melyn Were <WereMA@cardiff.ac.uk>
2026-02-05 15:05:23 +0000

    feat: enhance username input handling and validation in UsernameInput component

--- frontend/src/components/UsernameInput.tsx
+++ frontend/src/components/UsernameInput.tsx
@@ -1,17 +1,31 @@
 import '../styles/UsernameInput.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function UsernameInput() {
     const navigate = useNavigate();
+    const [username, setUsername] = useState('');  
     
     const handleJoin = () => {
+      if (username.trim() !== '') {
+        localStorage.setItem('username', username)
         navigate('/chat');
-    }
+      } else {
+        alert('Please enter a username before joining the chat.');
+      }
+    };
+
   return (
     <>
     <div className='input-form'>
           <form>
-            <input className='username-input' type="text" placeholder="Enter username" />
+            <input
+             className='username-input' 
+             type="text" 
+             placeholder="Enter username"
+             value={username}
+             onChange={(e) => setUsername(e.target.value)}
+              />
           </form>
           <button onClick={handleJoin}>JOIN</button>
     </div>
