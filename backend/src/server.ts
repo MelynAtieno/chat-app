1b0c20f42722075a53a34537adaa7f721cc3ce47
Melyn Were <WereMA@cardiff.ac.uk>
2026-02-06 14:24:20 +0000

    feat: integrate dotenv for environment variable management and connect to MongoDB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+import { Message } from './models/message';
 
-
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const httpServer = createServer(app);
@@ -16,6 +19,11 @@ app.use(express.json());
 app.get('/',(req, res) => {
     res.send('Server is running!');
 });
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI!)
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // Start the server
 httpServer.listen(PORT, () => {
@@ -31,11 +39,30 @@ const io = new Server(httpServer, {
 });
 
 // Listen for socket connections
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('User connected!:', socket.id); //Each connected client gets a unique socket object with its own id.
+
+    // Send chat history to the new user
+    try{
+        const messages = await Message.find().sort({ createdAt: 1 }).limit(100); // Fetch the last 100 messages, sorted by creation time
+        socket.emit('chat history', messages);
+    } catch (err) {
+        console.error('Error fetching chat history:', err);
+    }
     // Listen for chat messages and broadcast them to all connected clients
-    socket.on('chat message', (msg) => {
+    socket.on('chat message', async (msg) => {
         console.log('Message received:', msg);
+        
+        // Save the message to the database
+        try {
+            const newMessage = new Message(msg);
+            await newMessage.save();
+            console.log('Message saved to database!');
+        } catch (err) {
+            console.error('Error saving message to database:', err);
+        }
         io.emit('chat message', msg)// Broadcast the message to all connected clients
+
     })
+    
 });
